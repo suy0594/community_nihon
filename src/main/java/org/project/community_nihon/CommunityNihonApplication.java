8e5c89011d23c91b9d756af95de432aa5d4c8253
suy0594 <tndud25@gmail.com>
2024-05-13 14:17:13 +0900

    Initial commit

---
+++ src/main/java/org/project/community_nihon/CommunityNihonApplication.java
@@ -0,0 +1,13 @@
+package org.project.community_nihon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityNihonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityNihonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/community_nihon/CommunityNihonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.community_nihon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityNihonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
