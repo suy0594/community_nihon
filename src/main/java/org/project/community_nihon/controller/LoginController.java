e9e53c7c60c491ea0befd37ca6e550351d69f144
suy0594 <tndud25@gmail.com>
2024-05-16 14:15:42 +0900

    react 추가

--- src/main/java/org/project/community_nihon/controller/LoginController.java
+++ src/main/java/org/project/community_nihon/controller/LoginController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@RestController
 @Controller
 @Log4j2
 @RequiredArgsConstructor
